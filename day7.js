1ddd8de789a8e6d51dd6ea407275cc86c7bb73a8
Starsky Torchia <startork@gmail.com>
2020-12-07 16:12:28 +0000

    Update day7.js

--- day7.js
+++ day7.js
@@ -1,14 +1,15 @@
+var allBags = {};
+
 function runFunction() {
   document.getElementById("inputText").addEventListener("change", function () {
     var fr = new FileReader();
     fr.onload = function () {
-      var lines = this.result.split("\r\n");
-      var allBags = {};
+      var lines = this.result.split(".\r\n");
       lines.forEach((line) => {
         var [bag, bags] = line.split(" bags contain ");
         var bagsObject = {};
         bags.split(", ").forEach((bagsbag) => {
-          bagsbag = bagsbag.replace(/( bags.| bag.| bags| bag)/g, "");
+          bagsbag = bagsbag.replace(/( bags| bag)/g, "");
           var fart = bagsbag.split(" ");
           bagsObject[fart.shift()] = fart.join(" ");
         });
@@ -16,8 +17,9 @@ function runFunction() {
       });
 
       var fun = Object.keys(allBags).filter((obj) =>
-        containsGold(allBags[obj], allBags)
+        containsGold(allBags[obj])
       );
+      console.log(Object.keys(allBags).length);
       console.log(fun.length);
     };
 
@@ -25,14 +27,14 @@ function runFunction() {
   });
 }
 
-function containsGold(object, happyBags) {
+function containsGold(object) {
   if (!object) {
     return false;
   } else if (Object.values(object).includes("shiny gold")) {
     return true;
   } else {
     return Object.values(object).some((el) => {
-      return containsGold(happyBags[el], happyBags);
+      return containsGold(allBags[el]);
     });
   }
 }
