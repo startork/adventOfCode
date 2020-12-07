e04143c6a4a25e034af505404259724d9e99bc71
startork <starsky.t@bluefusesystems.com>
2020-12-07 17:51:49 +0000

    Update day6.js

--- day6.js
+++ day6.js
@@ -9,7 +9,7 @@ function runFunction() {
         groups = lines.join('').split('&&');
         groups.forEach((element, i) => {
           groups[i] = new Set(element).size;
-        })
+        });
         const answerStep1 = groups.reduce((a, b) => a + b, 0)
         
         console.log(answerStep1);
@@ -21,13 +21,13 @@ function runFunction() {
         groups = lines.join('&').split('&&&');
         groups.forEach((element, i) => {
           groups[i] = element.split('&');
-        })
+        });
         groupYes = groups.map(element => {
           var arrayNum = element.reduce((a, b) => a.filter(element => [...b].includes(element)), [...element[0]]);
-          return arrayNum.length
-        })
+          return arrayNum.length;
+        });
 
-        const answerStep2 = groupYes.reduce((a, b) => a + b, 0)
+        const answerStep2 = groupYes.reduce((a, b) => a + b, 0);
         console.log(answerStep2);
       };
   
