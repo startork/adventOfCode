ce1d735bc7c235c724888ff364fba4dba57f5cfc
startork <starsky.t@bluefusesystems.com>
2020-12-07 18:47:05 +0000

    Update day7.js

--- day7.js
+++ day7.js
@@ -9,32 +9,43 @@ function runFunction() {
         var [bag, bags] = line.split(" bags contain ");
         var bagsObject = {};
         bags.split(", ").forEach((bagsbag) => {
-          bagsbag = bagsbag.replace(/( bags| bag)/g, "");
+          bagsbag = bagsbag.replace(/( bags.| bag.| bags| bag)/g, "");
           var fart = bagsbag.split(" ");
-          bagsObject[fart.shift()] = fart.join(" ");
+          const amount = fart.shift();
+          bagsObject[fart.join(" ")] = parseInt(amount);
         });
         allBags[bag] = bagsObject;
       });
 
-      var fun = Object.keys(allBags).filter((obj) =>
-        containsGold(allBags[obj])
-      );
-      console.log(Object.keys(allBags).length);
-      console.log(fun.length);
+      var withGold = Object.keys(allBags).filter((name) => calculateGold(allBags[name]));
+
+      console.log(withGold.length);
+
+      console.log(calculateBags(allBags['shiny gold']));
     };
 
     fr.readAsText(this.files[0]);
   });
 }
 
-function containsGold(object) {
-  if (!object) {
-    return false;
-  } else if (Object.values(object).includes("shiny gold")) {
-    return true;
-  } else {
-    return Object.values(object).some((el) => {
-      return containsGold(allBags[el]);
-    });
-  }
+function calculateGold(object) {
+  return Object.keys(object).some((name) => {
+    if (name === 'shiny gold') {
+      return true;
+    } else if (name === 'other') {
+      return false;
+    } else {
+      return calculateGold(allBags[name]);
+    }
+  })
+}
+
+function calculateBags(object) {
+  return Object.keys(object).map((name) => {
+    if (name === 'other') {
+      return 0;
+    } else {
+      return object[name] + object[name] * calculateBags(allBags[name]);
+    }
+  }).reduce((a, b) => a + b, 0);
 }
