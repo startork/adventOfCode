4d1adccfb93c7428cba488e40172f96bf9b20e36
Joshua Galpin <josh.g@bluefusesystems.com>
2020-12-16 20:31:53 +0000

    gtyuktyugyhug

--- day16.js
+++ day16.js
@@ -1,11 +1,12 @@
-const fs = require("fs");
-const text = fs.readFileSync("input.txt", { encoding: "utf-8" });
-const lines = text.split("\r\n");
+const fs = require("fs")
+const text = fs.readFileSync("input.txt", { encoding: "utf-8" })
+const lines = text.split("\r\n")
 
 const ruleNumbers = lines
   .slice(0, 20)
   .join("\n")
-  .match(/([0-9]+)/g);
+  .match(/([0-9]+)/g)
+
 const rules = ruleNumbers
   .map((el, i) => {
     if (i % 4 === 0) {
@@ -19,47 +20,41 @@ const rules = ruleNumbers
         " && " +
         ruleNumbers[i + 3] +
         " >= ruleInput"
-      );
+      )
     }
   })
-  .filter((x) => x);
+  .filter((x) => x)
 
-const nearbyIndex = lines.findIndex((x) => x === "nearby tickets:");
-const nearbyTickets = lines.slice(nearbyIndex + 1);
-const numbers = nearbyTickets.join(",").split(",");
+const nearbyIndex = lines.findIndex((x) => x === "nearby tickets:")
+const nearbyTickets = lines.slice(nearbyIndex + 1)
+const numbers = nearbyTickets.join(",").split(",")
 
 const naughtyNumbers = numbers.filter((ruleInput) => {
   return !rules.some((rule) => {
-    return eval(rule);
-  });
-});
+    return eval(rule)
+  })
+})
 
 // Step One
-console.log(naughtyNumbers.reduce((a, b) => a + parseInt(b), 0));
+console.log(naughtyNumbers.reduce((a, b) => a + parseInt(b), 0))
 
 const validTickets = nearbyTickets
-  .filter(
-    (ticket) =>
-      !ticket.split(",").some((ruleInput) => !rules.some((rule) => eval(rule)))
+  .map((x) => x.split(","))
+  .filter((ticket) =>
+    ticket.every((ruleInput) => rules.some((rule) => eval(rule)))
   )
-  .map((x) => x.split(","));
 
-locationObject = {};
+locationObject = {}
 for (i = 0; i < validTickets[0].length; i++) {
-  const ruleIndex = rules.findIndex((rule) =>
-    validTickets.every((ticket) => {
-      const ruleInput = ticket[i];
-      return eval(rule);
-    })
-  );
-  locationObject[i] = ruleIndex;
+  const ruleIndex = rules
+    .filter((rule) =>
+      validTickets.every((ticket) => {
+        const ruleInput = ticket[i]
+        return eval(rule)
+      })
+    )
+    .map((x) => rules.findIndex((r) => r === x))
+  locationObject[i] = ruleIndex
 }
-console.log(
-  validTickets.every((ticket) => {
-    const ruleInput = ticket[0];
-    return (
-      (ruleInput >= 44 && 825 >= ruleInput) ||
-      (ruleInput >= 849 && 962 >= ruleInput)
-    );
-  })
-);
+
+console.log(locationObject)
