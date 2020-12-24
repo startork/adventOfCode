52c3404b057bd5b8bbf6b811f350ccca150aa135
Starsky Torchia <startork@gmail.com>
2020-12-24 11:49:34 +0000

    day24

--- day18.js
+++ day18.js
@@ -28,5 +28,40 @@ function calculateLine(line) {
 
 let sum = 0;
 lines.forEach((line) => (sum += calculateLine(line)[0]));
+//part one
+console.log(sum);
 
+function formatLine(line) {
+  let acc = 0;
+  let operator = "+";
+  for (var x = 0, letter = ""; (letter = line.charAt(x)); x++) {
+    switch (true) {
+      case /[0-9]/g.test(letter):
+        if (operator === "+") {
+          acc = eval(acc + operator + letter);
+        } else {
+          const result = calculateLine(line.substring(x + 1));
+          x += result[1] + 1;
+          acc = eval(acc + operator + result[0]);
+        }
+        break;
+      case /\+|\*/.test(letter):
+        operator = letter;
+        break;
+      case letter === "(":
+        const result = calculateLine(line.substring(x + 1));
+        x += result[1] + 1;
+        acc = eval(acc + operator + result[0]);
+        break;
+      case letter === ")":
+        return [acc, x];
+    }
+  }
+
+  return [acc, x];
+}
+
+let partTwoSum = 0;
+lines.forEach((line) => (partTwoSum += calculatePartTwoLine(line)[0]));
+//part one
 console.log(sum);
