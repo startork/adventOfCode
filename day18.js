bb5aafedd4f1abc94ce45ca4be5ba86d5a9e08e2
startork <starsky.t@bluefusesystems.com>
2021-01-09 18:23:33 +0000

    day18 part Two

--- day18.js
+++ day18.js
@@ -31,37 +31,36 @@ lines.forEach((line) => (sum += calculateLine(line)[0]));
 //part one
 console.log(sum);
 
-function formatLine(line) {
-  let acc = 0;
-  let operator = "+";
-  for (var x = 0, letter = ""; (letter = line.charAt(x)); x++) {
-    switch (true) {
-      case /[0-9]/g.test(letter):
-        if (operator === "+") {
-          acc = eval(acc + operator + letter);
-        } else {
-          const result = calculateLine(line.substring(x + 1));
-          x += result[1] + 1;
-          acc = eval(acc + operator + result[0]);
-        }
-        break;
-      case /\+|\*/.test(letter):
-        operator = letter;
-        break;
-      case letter === "(":
-        const result = calculateLine(line.substring(x + 1));
-        x += result[1] + 1;
-        acc = eval(acc + operator + result[0]);
-        break;
-      case letter === ")":
-        return [acc, x];
-    }
+function doAddition(line) {
+  while (/[0-9] \+ [0-9]/g.test(line)) {
+    matches = line.match(/[0-9]+ \+ [0-9]+/g);
+    line = line.replace(matches[0], eval(matches[0]) + "");
   }
-
-  return [acc, x];
+  return line;
 }
 
-let partTwoSum = 0;
-lines.forEach((line) => (partTwoSum += calculatePartTwoLine(line)[0]));
-//part one
-console.log(sum);
+function doMultiplication(line) {
+  while (/[0-9] \* [0-9]/g.test(line)) {
+    matches = line.match(/[0-9]+ \* [0-9]+/g);
+    line = line.replace(matches[0], eval(matches[0]) + "");
+  }
+  return line;
+}
+
+function simplifyBrackets(line) {
+  while (/\([0-9 \*\+]+\)/g.test(line)) {
+    matches = line.match(/\([0-9 \*\+]+\)/g);
+    line = line.replace(matches[0], eval(matches[0]) + "");
+    line = doAddition(line);
+  }
+  return line;
+}
+
+results = lines.map((line) => {
+  line = doAddition(line);
+  line = simplifyBrackets(line);
+  line = doMultiplication(line);
+  return parseInt(line);
+});
+//part two
+console.log(results.reduce((a, b) => a + b, 0));
