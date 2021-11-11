c6e9c53ce9f865c9e2c2ffb701d5aecc7ecd00e5
startork <startork@gmail.com>
2021-11-11 18:56:03 +0000

    Update day19.js

--- day19/day19.js
+++ day19/day19.js
@@ -1,36 +1,33 @@
 const fs = require("fs");
 const text = fs.readFileSync("input.txt", { encoding: "utf-8" });
 const [rulesPartOne, input] = text.split("\n\n");
-const text2 = fs.readFileSync("input2.txt", { encoding: "utf-8"});
-const [rulesPartTwo] = text2.split('\n\n');
-const tests = input.split('\n');
+const text2 = fs.readFileSync("input2.txt", { encoding: "utf-8" });
+const [rulesPartTwo] = text2.split("\n\n");
+const tests = input.split("\n");
 
 //Part One V2
 const ruleObj1 = buildRuleObject(rulesPartOne);
-const ruleRegex1 = buildRuleRegex(ruleObj1, '0');
-const partOneMatches = tests.filter(str => ruleRegex1.test(str)).length;
-console.log('Part 1: ' + partOneMatches);
+const ruleRegex1 = buildRuleRegex(ruleObj1, "0");
+const partOneMatches = tests.filter((str) => ruleRegex1.test(str)).length;
+console.log("Part 1: " + partOneMatches);
 
 //Part Two
-const longest = tests.reduce(
-  function (a, b) {
-    return a.length > b.length ? a : b;
-  }
-).length;
+const longest = tests.reduce(function (a, b) {
+  return a.length > b.length ? a : b;
+}).length;
 const ruleObj2 = buildRuleObject(rulesPartTwo);
-const ruleRegex2 = buildRuleRegex(ruleObj2, '0');
-const matches2 = tests.filter(str => ruleRegex2.test(str)).length;
-console.log('Part 2: ' + matches2);
-
+const ruleRegex2 = buildRuleRegex(ruleObj2, "0");
+const matches2 = tests.filter((str) => ruleRegex2.test(str)).length;
+console.log("Part 2: " + matches2);
 
 function buildRuleObject(rules) {
   let object = {};
-  rules.split('\n').forEach(r => {
-    [key, d] = r.split(': ');
+  rules.split("\n").forEach((r) => {
+    [key, d] = r.split(": ");
     if (['"a"', '"b"'].includes(d)) {
-      object[key] = {'rule': d.replace(/"/g, ''), 'solved': true};
+      object[key] = { rule: d.replace(/"/g, ""), solved: true };
     } else {
-      object[key] = {'rule': d, 'solved': d === '"a"'};
+      object[key] = { rule: d, solved: d === '"a"' };
     }
   });
 
@@ -38,37 +35,37 @@ function buildRuleObject(rules) {
 }
 
 function buildRuleRegex(ruleObj, ruleKey) {
-  return new RegExp('^' + regexLoop(ruleKey).replace(/ /g, '') + '$');
+  return new RegExp("^" + regexLoop(ruleKey).replace(/ /g, "") + "$");
 
   function regexLoop(ruleKey) {
     const rule = ruleObj[ruleKey];
     if (rule.solved) {
       return rule.rule;
-    } 
+    }
 
-    if (rule.rule.split(' ').includes(ruleKey)) {
+    if (rule.rule.split(" ").includes(ruleKey)) {
       //We have a loop!!
       //Only used for part TWO
       if (ruleKey == 8) {
         //For rule 8 we can just use regex trickery
-        rule.rule = '(?:' + regexLoop('42') + ')+';
+        rule.rule = "(?:" + regexLoop("42") + ")+";
       } else {
-        //Rule 11 is essentially a palindrome which we know is 
+        //Rule 11 is essentially a palindrome which we know is
         //impossible to test for with raw regex
-        const rule42 = regexLoop('42').replace(/ /g, '');
-        const rule31 = regexLoop('31').replace(/ /g, '');
-        rule.rule = `${rule42}${rule31}`
+        const rule42 = regexLoop("42").replace(/ /g, "");
+        const rule31 = regexLoop("31").replace(/ /g, "");
+        rule.rule = `${rule42}${rule31}`;
         //build rules assuming that the smallest combination of 42 and 31 is two letters
-        for (let i = 2; i < longest/2 + 2; i++) {
+        for (let i = 2; i < longest / 2 + 2; i++) {
           rule.rule = `${rule.rule}|${rule42.repeat(i)}${rule31.repeat(i)}`;
         }
-        rule.rule = '(?:' + rule.rule + ')';
+        rule.rule = "(?:" + rule.rule + ")";
       }
     } else {
-      rule.rule = '(?:' + rule.rule.replace(/[0-9]+/ig, regexLoop) + ')';
+      rule.rule = "(?:" + rule.rule.replace(/[0-9]+/gi, regexLoop) + ")";
     }
-    
+
     rule.solved = true;
-    return rule.rule
-  } 
+    return rule.rule;
+  }
 }
