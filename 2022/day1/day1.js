8eb465cc713fabf767a9aa0c29df680fceb3c82b
Starsky Torchia <startork@gmail.com>
2022-12-01 18:15:40 +0000

    Messy but it works

---
+++ 2022/day1/day1.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const text = fs.readFileSync("input.txt", { encoding: "utf-8" });
+
+//part one
+const elfs = text.split("\r\n\r\n");
+
+const elfTotals = elfs.map((string) => {
+  return string.split("\r\n").reduce((a, b) => Number(a) + Number(b), 0);
+});
+
+console.log(Math.max(...elfTotals));
+
+// Part Two
+const topThree = elfTotals.reduce(
+  (a, b) => {
+    if (b > a[2]) {
+      a[0] = a[1];
+      a[1] = a[2];
+      a[2] = b;
+    } else if (b > a[1]) {
+      a[0] = a[1];
+      a[1] = b;
+    } else if (b > a[0]) {
+      a[0] = b;
+    }
+    return a;
+  },
+  [0, 0, 0]
+);
+
+console.log(topThree[0] + topThree[1] + topThree[2]);
